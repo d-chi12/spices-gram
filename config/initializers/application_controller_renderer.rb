9bb65ff7dba1be828d4f8da8130e93c6d1718dd8
USER <k.k.fliper12@gmail.com>
2020-07-01 12:19:25 +0900

    Initial commit

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,11 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+
+Refile.secret_key = '34c4c813a798c40b2f8f2a40c3128fb6f5114612837567c4b7cfce90d1c2f7005f517cb9660f776b56cce2e4eb8aa25b6c1ee39d9a969f2386897b2ccba1d6af'
---
+++ db/migrate/20200525051529_add_colums_to_user.rb
@@ -0,0 +1,7 @@
+class AddColumsToUser < ActiveRecord::Migration[6.0]
+  def change
+    add_column :users, :username, :string
+    add_column :users, :profile, :text
+    add_column :users, :profile_image_id, :string
+  end
+end
