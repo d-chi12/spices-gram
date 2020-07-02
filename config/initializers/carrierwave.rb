d1caf5681f92a4254a1e6fa24cd2135c110249ac
USER <k.k.fliper12@gmail.com>
2020-07-02 11:03:02 +0900

    s3 key 環境変数

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,13 +1,11 @@
-unless Rails.env.development? || Rails.env.test?
-  CarrierWave.configure do |config|
-    config.fog_credentials = {
-      provider: 'AWS',
-      aws_access_key_id: ENV['ACCESS_KEY'],
-      aws_secret_access_key: ENV['SECRET_KEY'],
-      region: 'ap-northeast-1'
-    }
+CarrierWave.configure do |config|
+  config.fog_credentials = {
+    provider: 'AWS',
+    aws_access_key_id: ENV['ACCESS_KEY'],
+    aws_secret_access_key: ENV['SECRET_KEY'],
+    region: 'ap-northeast-1'
+  }
 
-    config.fog_directory  = ENV['FOG_DIRECTORY']
-    config.cache_storage = :fog
-  end
+  config.fog_directory  = ENV['FOG_DIRECTORY']
+  config.cache_storage = :fog
 end
