718c178dfc76ea5e91416bed7ba3dbcc49c63e6c
USER <k.k.fliper12@gmail.com>
2020-07-07 18:11:55 +0900

    add intro

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+  get 'intro/index'
+  get 'intro/show'
   devise_for :users
   root to: "home#index"
   resources :users
   resources :recipes
+  resources :intro
+  
 end
