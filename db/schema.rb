92abff66915e9f67dfc1d5b028baca850289c51a
USER <k.k.fliper12@gmail.com>
2020-07-09 12:14:17 +0900

    add favorite 設定

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_02_005352) do
+ActiveRecord::Schema.define(version: 2020_07_09_030401) do
+
+  create_table "favorites", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "recipe_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "photos", force: :cascade do |t|
     t.string "image"
