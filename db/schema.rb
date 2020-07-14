940e676714d90e0c67710eea8e71040aacb0e348
USER <k.k.fliper12@gmail.com>
2020-07-14 23:50:38 +0900

    recipe アクセス制限

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_09_093726) do
+ActiveRecord::Schema.define(version: 2020_07_11_075301) do
 
   create_table "comments", force: :cascade do |t|
     t.string "content"
@@ -33,6 +33,15 @@ ActiveRecord::Schema.define(version: 2020_07_09_093726) do
     t.string "image"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.string "image"
+    t.boolean "active"
+    t.integer "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "recipes", force: :cascade do |t|
