d5393e93e5b862afdac64aa58905055a4b2668ed
USER <k.k.fliper12@gmail.com>
2020-09-07 13:18:39 +0900

    intro js topページに戻る

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,12 +7,10 @@
 //= require turbolinks
 //= require_tree .
 
-
-require("@rails/ujs").start()
-require("turbolinks").start()
-require("@rails/activestorage").start()
-require("channels")
-
+require("@rails/ujs").start();
+require("turbolinks").start();
+require("@rails/activestorage").start();
+require("channels");
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
@@ -34,3 +32,38 @@ require("channels")
 //       }
 //   });
 // });
+
+/* ========================================================
+スクロールでトップに戻るボタンを表示
+=========================================================*/
+// スクロールして何ピクセルでアニメーションさせるか
+var px_change = 1;
+// スクロールのイベントハンドラを登録
+window.addEventListener("scroll", function (e) {
+  // 変化するポイントまでスクロールしたらクラスを追加
+  var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+  if (scrollTop > px_change) {
+    document.getElementById("btn-backtotop").classList.add("fadein");
+
+    // 変化するポイント以前であればクラスを削除
+  } else {
+    document.getElementById("btn-backtotop").classList.remove("fadein");
+  }
+});
+
+/* ========================================================
+トップに戻るボタンのスムーズスクロール
+=========================================================*/
+
+document
+  .getElementById("btn-backtotop")
+  .addEventListener("click", function (e) {
+    anime.remove("html, body");
+    anime({
+      targets: "html, body",
+      scrollTop: 0,
+      dulation: 600,
+      easing: "easeOutCubic",
+    });
+    return false;
+  });
