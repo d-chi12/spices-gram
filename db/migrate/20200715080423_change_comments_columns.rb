1beb4bb1528b6bb926554e321e900a775eba8eda
d-chi12 <k.k.fliper12@gmail.com>
2020-10-28 15:51:12 +0900

    migrate 削除

--- db/migrate/20200715080423_change_comments_columns.rb
+++
@@ -1,7 +0,0 @@
-class ChangeCommentsColumns < ActiveRecord::Migration[6.0]
-  def change
-    remove_column :comments, :user, :references
-    remove_column :comments, :recipe, :references
-    remove_column :comments, :content, :string
-  end
-end
--- db/migrate/20200715084042_add_columns_to_comments.rb
+++
@@ -1,4 +0,0 @@
-class AddColumnsToComments < ActiveRecord::Migration[6.0]
-  def change
-  end
-end
